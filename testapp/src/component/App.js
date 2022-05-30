fbde418ab6f9a918a15e1a360d0c40e4f79d9389
kaito <mokos.cook@gmail.com>
2022-05-30 16:27:10 +0900

    osimai

--- testapp/src/component/App.js
+++ testapp/src/component/App.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from "react"
+import React, { useEffect, useReducer } from "react"
 import { Container } from '@mui/material'
 
 import EventForm from "./EventForm"
@@ -7,12 +7,19 @@ import OperationLogs from "./OperationLogs"
 import reducer from '../reducers'
 import AppContext from "../contexts/AppContext"
 
+const APP_KEY = 'appWithRedux'
+
 const App = () => {
-  const initialState = {
+  const appState = localStorage.getItem(APP_KEY)
+  const initialState = appState ? JSON.parse(appState) : {
     events: [],
     operationLogs: []
   }
   const [state, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(() => {
+    localStorage.setItem(APP_KEY, JSON.stringify(state))
+  }, [state])
 
   return (
     <AppContext.Provider value={{ state, dispatch }}>
